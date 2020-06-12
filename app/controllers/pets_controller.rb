b26507dc84c839af45f68127d11508d30edfd080
Jack Madden <jck13mad@gmail.com>
2020-06-12 20:33:51 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -13,7 +13,7 @@ class PetsController < ApplicationController
   post '/pets' do 
     @owners = Owner.all
     @pet = Pet.create(params[:pet])
-    if !params
+    if !params["owner"]["pet"]
     
     redirect to "pets/#{@pet.id}"
   end
