a65ae2472bd3c561c81ee7c8264e82d3b89e9656
Jack Madden <jck13mad@gmail.com>
2020-06-12 20:36:09 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -25,7 +25,8 @@ class PetsController < ApplicationController
   get '/pets/:id' do 
     @pet = Pet.find(params[:id])
     @pet.update(params[:pet])
-    if !params["owner"]["id"]
+    if !params["owner"]["name"].empty?
+      
     
     erb :'/pets/show'
   end
